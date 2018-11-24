aab05fed3bf9bf611b9e3136fbb5e5edd490e1ce
Alex Jacobs <alexander.jacobs@protonmail.ch>
2018-11-24 13:33:19 +0200

    Added firebase example and config settings to constant folder

---
+++ Frontend/constants/Firebase.js
@@ -0,0 +1,9 @@
+// Configuration settings for Firebase instance
+export const firebase_config = {
+  apiKey: "AIzaSyCwa1Tds6ig7KsltrVvvMsMx7-zLK9UgrY",
+  authDomain: "junction2018-223415.firebaseapp.com",
+  databaseURL: "https://junction2018-223415.firebaseio.com",
+  projectId: "junction2018-223415",
+  storageBucket: "junction2018-223415.appspot.com",
+  messagingSenderId: "429487068420"
+};
