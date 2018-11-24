10781bab75e315ffb12e0025c0974037416259c6
Alex Jacobs <alexander.jacobs@protonmail.ch>
2018-11-24 15:27:02 +0200

    integrated firebase

--- Frontend/constants/Firebase.js
+++ Frontend/constants/Firebase.js
@@ -1,5 +1,5 @@
 // Configuration settings for Firebase instance
-export const firebase_config = {
+export default firebase_config = {
   apiKey: "AIzaSyCwa1Tds6ig7KsltrVvvMsMx7-zLK9UgrY",
   authDomain: "junction2018-223415.firebaseapp.com",
   databaseURL: "https://junction2018-223415.firebaseio.com",
