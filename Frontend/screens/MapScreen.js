209bdc7c79ac9b965510fa1d8bb6677e4d47a6be
heikunKeikun <jjouhansson@hotmail.fi>
2018-11-25 04:46:29 +0200

    frankensteined firebase and api results to single state array

--- Frontend/screens/MapScreen.js
+++ Frontend/screens/MapScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button, KeyboardAvoidingView, Alert, YellowBox } from 'react-native';
 import { MapView, Location, Permissions } from 'expo';
-const key = 'AIzaSyB5LPlTnHClwSE8rXgznk6nuGxxBnAfu1M';
 import * as firebase from "firebase";
 
 YellowBox.ignoreWarnings(['Setting a timer']);
@@ -9,23 +8,24 @@ const base64 = require('base-64');
 
 const info = [
   {
-    name :"pasila",
-    lat :60.1990,
-    lon:24.9328,
-    chargingPointGroup :[
-      {  id : "4",
-        chargePointVendor:"Ensto",
-        chargingPoint:[
+    name: "pasila",
+    lat: 60.1990,
+    lon: 24.9328,
+    chargingPointGroup: [
+      {
+        id: "4",
+        chargePointVendor: "Ensto",
+        chargingPoint: [
           {
-            id:"1",
-            status:"available"
+            id: "1",
+            status: "available"
           }
         ]
 
       }
     ],
 
-    status:"available"
+    status: "available"
   }
 
 ]
@@ -58,13 +58,13 @@ export default class MapScreen extends React.Component {
       longitudeDelta: 0.1,
       markerTitle: '',
       markers: [],
-      description:'boomboi',
-      chargingPointGroups: {},
-      ensto: []
+      description: 'boomboi',
+      EVChargers: [],
+      chargingPointGroups: [],
     }
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.getLocation();
     this.enstoAPI();
   }
@@ -87,62 +87,80 @@ export default class MapScreen extends React.Component {
     await fetch("https://junctionev.enstoflow.com/api/v1/chargingPointGroup", {
       method: 'GET',
       headers: new Headers({
-        "Authorization": "Basic " + base64.encode(username+":"+pswd),
+        "Authorization": "Basic " + base64.encode(username + ":" + pswd),
         "Content-Type": "application/json"
       })
     })
-    .then((response) => {
-      if (!response.ok) {
-        console.log("Oops! Something went wrong!")
-      }
-      else
-      return response.json()
-    })
-    .then((responseJSON) => {
-      /*for (var i = 0; i < responseJSON.length; i++) {
-      console.log(`chargingPointGroup id: ${responseJSON[i].id}`);
-
-      for (var j = 0; j < responseJSON[i].chargingPoints.length; j++) {
-      console.log(`  ${responseJSON[i].chargingPoints[j].chargePointVendor} ${responseJSON[i].chargingPoints[j].chargePointModel}`);
-
-      for (var k = 0; k < responseJSON[i].chargingPoints[j].connectors.length; k++) {
-      if (responseJSON[i].chargingPoints[j].connectors[k].id === 0) {
-      continue
-      }
-      console.log(`    connector: ${responseJSON[i].chargingPoints[j].connectors[k].id}, status: ${responseJSON[i].chargingPoints[j].connectors[k].status}`);
-      }
-      }
-      console.log('\n');
-      }*/
-      this.setState({
-        ensto: JSON.stringify(responseJSON, null, 2)
+      .then((response) => {
+        if (!response.ok) {
+          console.log("Oops! Something went wrong!")
+        }
+        else
+          return response.json()
       })
-      console.log('enstoAPI()');
-    })
-    .then(this.getChargingPointGroups);
+      .then((responseJSON) => {
+        for (var i = 0; i < responseJSON.length; i++) {
+          let tempChargingPointGroup = {};
+          tempChargingPointGroup.id = responseJSON[i].id;
+          tempChargingPointGroup.chargingPoints = [];
+
+          for (var j = 0; j < responseJSON[i].chargingPoints.length; j++) {
+            let tempChargingPoint = {};
+            tempChargingPoint.id = responseJSON[i].chargingPoints[j].id;
+            tempChargingPoint.chargePointVendor = responseJSON[i].chargingPoints[j].chargePointVendor;
+            tempChargingPoint.chargePointModel = responseJSON[i].chargingPoints[j].chargePointModel;
+            tempChargingPoint.connectors = [];
+            tempChargingPointGroup.chargingPoints.push(tempChargingPoint);
+
+            for (var k = 0; k < responseJSON[i].chargingPoints[j].connectors.length; k++) {
+              let tempConnector = {};
+              if (responseJSON[i].chargingPoints[j].connectors[k].id === 0) {
+                continue
+              }
+              tempConnector.connectorId = responseJSON[i].chargingPoints[j].connectors[k].id;
+              tempConnector.status = responseJSON[i].chargingPoints[j].connectors[k].status;
+              tempChargingPointGroup.chargingPoints[j].connectors.push(tempConnector);
+            }
+          }
+          this.setState({EVChargers: [...this.state.EVChargers, tempChargingPointGroup]});
+        }
+      })
+      .then(this.getChargingPointGroups);
   }
 
   getChargingPointGroups = () => {
-    return firebase.database().ref(`/chargingPointGroup`).once('value').then(function(snapshot) {
-      this.setState({
-        chargingPointGroups: snapshot
+    let firebaseResult = [];
+    let temp = this.state.EVChargers;
+    let newArr = [];
+    
+    return firebase.database().ref(`/chargingPointGroup`).once('value').then(function (snapshot) {
+      for (let ele of snapshot.val()) {
+        if (ele) {
+          firebaseResult.push(ele);
+        }
+      }
+
+      firebaseResult.sort(function(a, b){return a.id - b.id});
+      temp.sort(function(a, b){return a.id - b.id});
+
+      firebaseResult.forEach((itm, i) => {
+        newArr.push(Object.assign({}, itm, temp[i]));
       });
-      console.log('getChargingPointGroups()');
-      console.log(this.state.chargingPointGroups);
+      this.setState({EVChargers: newArr});
     }.bind(this));
   }
 
   render() {
 
     const renderMarkers = info.map((info, index) =>
-    <MapView.Marker
-      key = {index}
-      title = {info.name}
-      description={info.status}
-      coordinate = {{
-        latitude: info.lat,
-        longitude: info.lon
-      }} />
+      <MapView.Marker
+        key={index}
+        title={info.name}
+        description={info.status}
+        coordinate={{
+          latitude: info.lat,
+          longitude: info.lon
+        }} />
     )
 
     return (
@@ -160,13 +178,9 @@ export default class MapScreen extends React.Component {
               latitude: this.state.latitude,
               longitude: this.state.longitude
             }}
-            title= {this.state.markerTitle} />
+            title={this.state.markerTitle} />
           {renderMarkers}
         </MapView>
-        <View style={styles.search}>
-          <TextInput style={{fontSize: 18, height: 50}} value={this.state.address}
-            onChangeText={(address) => this.setState({address})} />
-        </View>
       </KeyboardAvoidingView>
     );
   }
@@ -178,10 +192,6 @@ const styles = StyleSheet.create({
     flex: 1
   },
   map: {
-    flex: 0.8
-  },
-  search: {
-    flex: 0.2,
-    padding: 5
+    flex: 1
   }
 });
